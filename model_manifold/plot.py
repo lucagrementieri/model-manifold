b394d01fc3a49d9e785656037dfb33a89bf87a78
Luca Grementieri <lgrementieri@nextb.it>
2020-09-28 09:58:20 +0200

    :sparkles: Add export to gif

---
+++ model_manifold/plot.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import Union, Optional
+
+import imageio
+import torch
+from torchvision import transforms
+
+
+def to_gif(images: torch.Tensor, output_path: Union[str, Path]) -> None:
+    # noinspection PyArgumentList
+    images = images.permute(0, 2, 3, 1).squeeze_(-1)
+    images = torch.round(images * 255).to(torch.uint8)
+    images = torch.unbind(images)
+    imageio.mimsave(str(output_path), images)
+
+
+def denormalize(x: torch.Tensor, normalization: transforms.Normalize) -> torch.Tensor:
+    mean = torch.tensor(normalization.mean).view(-1, 1, 1)
+    std = torch.tensor(normalization.std).view(-1, 1, 1)
+    x *= std
+    x += mean
+    return x
+
+
+def evolution_to_gif(
+        evolution: torch.Tensor,
+        output_path: Union[str, Path],
+        normalization: Optional[transforms.Normalize] = None,
+) -> None:
+    if normalization is not None:
+        evolution = denormalize(evolution, normalization)
+    to_gif(evolution, output_path)
