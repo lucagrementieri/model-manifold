bdfcd2372a2544b88546ebaf8308f60768d72e28
Luca Grementieri <lgrementieri@nextb.it>
2020-09-28 10:31:59 +0200

    :bug: Fix device

--- model_manifold/plot.py
+++ model_manifold/plot.py
@@ -10,13 +10,14 @@ def to_gif(images: torch.Tensor, output_path: Union[str, Path]) -> None:
     # noinspection PyArgumentList
     images = images.permute(0, 2, 3, 1).squeeze_(-1)
     images = torch.round(images * 255).to(torch.uint8)
+    images = images.cpu()
     images = torch.unbind(images)
     imageio.mimsave(str(output_path), images)
 
 
 def denormalize(x: torch.Tensor, normalization: transforms.Normalize) -> torch.Tensor:
-    mean = torch.tensor(normalization.mean).view(-1, 1, 1)
-    std = torch.tensor(normalization.std).view(-1, 1, 1)
+    mean = torch.tensor(normalization.mean, device=x.device).view(-1, 1, 1)
+    std = torch.tensor(normalization.std, device=x.device).view(-1, 1, 1)
     x *= std
     x += mean
     return x
